8bb192b0101f05068f97bced44d49ab52eabd215
razuara <rodolfo.azuara@gmail.com>
2022-05-01 23:01:31 -0500

    Inicio de Proyecto con Registro y Login Funcionando

---
+++ app/src/main/java/com/rocasoftware/rocaride/MainActivity.java
@@ -0,0 +1,114 @@
+package com.rocasoftware.rocaride;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView nuevoUsuario;
+    MaterialButton inicioSesion;
+    TextInputEditText emailEditText,passwordEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+        inicioSesion = findViewById(R.id.inicioSesion);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nuevoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        inicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+
+    }
+
+    private void validate() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            emailEditText.setError("Correo Invalido");
+            return;
+        }
+        else
+        {
+            emailEditText.setError(null);
+        }
+        if (password.isEmpty() || password.length() < 8)
+        {
+            passwordEditText.setError("Se necesitan mas de 8 caracteres");
+            return;
+        }
+        else if(!Pattern.compile("[0-9]").matcher(password).find())
+        {
+            passwordEditText.setError("Al menos un numero");
+            return;
+        }
+        else
+        {
+            passwordEditText.setError(null);
+        }
+        iniciarSesion(email,password);
+    }
+
+    private void iniciarSesion(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,"Credenciales equivocadas, trata de nuevo",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
